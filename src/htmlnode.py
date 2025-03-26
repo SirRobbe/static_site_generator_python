00ac4b5c6855701636140f7d5cde7cf541229dc6
SirRobbe <sirrobbe@protonmail.com>
2025-03-26 22:11:28 +0100

    added the html node

---
+++ src/htmlnode.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+class HTMLNode:
+    def __init__(self,
+                 tag: str = None,
+                 value: str = None,
+                 children: list[HTMLNode] = None,
+                 props: dict[str, str] = None):
+
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+
+    def to_html(self) -> str:
+        raise NotImplementedError()
+
+
+    def props_to_html(self) -> str:
+        prop_strings = list(map(lambda prop_key: f"{prop_key}=\"{self.props[prop_key]}\"", self.props))
+        return " ".join(prop_strings)
+
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from textnode import *
+from htmlnode import HTMLNode
 
 print("hello world")
 node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+
+child = HTMLNode(tag="a", value="link", props={"href": "https://www.boot.dev"})
+parent = HTMLNode(tag="h1", value="title", children=[child])
+
+print(parent)
