c77d4488a5d128ddaba96f11df5b638184d91054
SirRobbe <sirrobbe@protonmail.com>
2025-03-28 08:21:27 +0100

    implemented parent node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -27,3 +27,34 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict[str, str] = None):
+        super().__init__(tag, value, None, props)
+
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("LeafNode must have a value")
+
+        if self.tag is None:
+            return self.value
+
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list[HTMLNode], props: dict[str, str] = None):
+        super().__init__(tag, None, children, props)
+
+
+    def to_html(self) -> str:
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag")
+
+        if self.children is None:
+            raise ValueError("ParentNode must have children")
+
+        children_html = list(map(lambda child: child.to_html(), self.children))
+        return f"<{self.tag}{self.props_to_html()}>{"".join(children_html)}</{self.tag}>"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -22,3 +22,67 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode(tag="h1")
         props = node.props_to_html()
         self.assertEqual(props, "")
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+
+    def test_no_tag_to_html_p(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+
+    def test_leaf_with_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+
+    def test_to_html_to_children(self):
+        child_node1 = LeafNode("span", "child")
+        child_node2 = LeafNode("b", "bold")
+        parent_node = ParentNode("div", [child_node1, child_node2])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span><b>bold</b></div>")
+
+
+    def test_to_html_with_props(self):
+        child_node = LeafNode("b", "important")
+        parent_node = ParentNode("a", [child_node], props={"href": "https://test.com"})
+        self.assertEqual(parent_node.to_html(), '<a href="https://test.com"><b>important</b></a>')
+
+
+    def test_no_tag(self):
+        # None type here is ok because we test for invalid input
+        # noinspection PyTypeChecker
+        parent_node = ParentNode(None, [])
+        self.assertRaises(ValueError, parent_node.to_html)
+
+
+    def test_no_children(self):
+        # None type here is ok because we test for invalid input
+        # noinspection PyTypeChecker
+        parent_node = ParentNode("p", None)
+        self.assertRaises(ValueError, parent_node.to_html)
+
+
+    def test_empty_children(self):
+        parent_node = ParentNode("p", [])
+        self.assertEqual(parent_node.to_html(), '<p></p>')
